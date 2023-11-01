1c12a5ddef2b86242fcade8f299ed527a5973990
Obedience Moses <83711345+obediencemoses@users.noreply.github.com>
2023-11-01 23:04:52 +0100

    library page completed

--- src/pages/library/library.jsx
+++ src/pages/library/library.jsx
@@ -1,13 +1,14 @@
 import React from "react";
 import Header from "../../components/header/header";
-import BookSection from "../../components/book-section/bookSection";
+import BookSection from "../../components/book-section/librarySection";
 import Footer from "../../components/footer/footer";
 
 const Library = () => {
   return (
     <>
       <Header />
-      <BookSection />
+      <BookSection sectionTitle="Spiritual Books" navLinkTo="/books/spiritual" />
+      <BookSection sectionTitle="Academic Books" navLinkTo="/books/academics" />
       <Footer />
     </>
   );
