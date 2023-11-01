038d66ab68630caa70d75d450eeccd083364faaa
Obedience Moses <83711345+obediencemoses@users.noreply.github.com>
2023-11-01 22:04:54 +0100

    started the library page

--- src/pages/library/library.jsx
+++ src/pages/library/library.jsx
@@ -1,6 +1,6 @@
 import React from "react";
 import Header from "../../components/header/header";
-import BookSection from "../../components/book-section/bookSection"
+import BookSection from "../../components/book-section/bookSection";
 import Footer from "../../components/footer/footer";
 
 const Library = () => {
