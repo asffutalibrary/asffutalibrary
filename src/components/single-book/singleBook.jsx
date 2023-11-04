bb91b923e3a83aade268f95b6370c5abcabfdb95
Obedience Moses <83711345+obediencemoses@users.noreply.github.com>
2023-11-04 19:49:25 +0100

    Books uploaded and inerted into the code

--- src/components/single-book/singleBook.jsx
+++ src/components/single-book/singleBook.jsx
@@ -5,8 +5,8 @@ import Book from "../../assets/book.png"
 const SingleBook = ({ title = "Good morning holy spirit", author = "Hagin", coverImage = Book, bookLink }) => {
   return (
     <div className="single-book-container">
-      <a href={bookLink} download={title}>
-        <img src={coverImage} alt={coverImage} />
+      <a href={bookLink} download={title} rel="nofollow noreferrer noindex">
+        <img src={coverImage} alt={title} />
         <div>
           <h2>{title}</h2>
           <p>{author}</p>
